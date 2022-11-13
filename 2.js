797b841bb05ef4ae0bfc4352d8f7ff394321d67b
cwx <96339820+seetoclick@users.noreply.github.com>
2022-11-13 10:09:59 +0800

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,24 @@
+alert("cwx"+" good!")
+
+console.log("dududu".length);
+console.log("cwx is bang".charAt(1));
+console.log("cwx is bang".replace("cwx", "xwc"));
+
+var name = "cwx";
+console.log(name);
+
+let a = 2;
+console.log(a);
+
+a = a + 2;
+console.log(a);
+
+const PI = 3.14159265358;
+console.log(PI);
+
+if (PI == 3.14) {
+	console.log("pi is 3.14!");
+}
+else {
+	console.log("pi is not 3.14!");
+}
